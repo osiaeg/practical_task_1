b26937e63fd1e633534b57e97339ba9c101b9a75
osiaeg <egor.goncharov.osia@mail.ru>
2023-02-01 15:47:38 +0300

    test all message

--- src/client.py
+++ src/client.py
@@ -1,18 +1,18 @@
 from message import *
 
 if __name__ == '__main__':
+    messages = [
+            fast_response('127498127489127498124'),
+            slow_response(100),
+            request_for_fast_response(),
+            request_for_slow_response(1000),
+            ]
+
     with open('out.bin', 'wb') as f:
-        first_message = fast_response('127498127489127498124')
-        f.write(encode_varint(first_message))
-        f.write(first_message.SerializeToString())
-
-        second_message = slow_response(100)
-        f.write(encode_varint(second_message))
-        f.write(second_message.SerializeToString())
-
-        third_message = request_for_fast_response()
-        f.write(encode_varint(third_message))
-        f.write(third_message.SerializeToString())
+        for message in messages:
+            head_size = encode_varint(message)
+            package = head_size + message.SerializeToString()
+            f.write(package)
 
 
     with open('out.bin', 'rb') as f:
